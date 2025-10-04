b050edc3dc57a18cf6d356ffecd3cfd712194999
Thulhid <153322849+Thulhid@users.noreply.github.com>
2025-10-04 19:02:49 +0530

    fix: mobile logo

--- app/_components/AddCart.js
+++ app/_components/AddCart.js
@@ -1,8 +1,6 @@
 "use client";
 
 import Button from "@/app/_components/Button";
-import { CartContext } from "@/app/_context/CartContext";
-import { useContext } from "react";
 
 function AddCart({ product }) {
   // const { setSaveCartProduct } = useContext(CartContext);
